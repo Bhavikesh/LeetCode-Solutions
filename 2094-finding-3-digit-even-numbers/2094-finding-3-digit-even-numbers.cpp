26bbb8159f78e9d9646b613d0d964d5673145401
Bhavikesh <bhavikesh.h06@gmail.com>
2026-09-12 01:27:13 +0530

    Time: 7 ms (57.47%), Space: 15.3 MB (22.76%) - LeetHub

--- 2094-finding-3-digit-even-numbers/2094-finding-3-digit-even-numbers.cpp
+++ 2094-finding-3-digit-even-numbers/2094-finding-3-digit-even-numbers.cpp
@@ -1,28 +1,35 @@
 class Solution {
 public:
     vector<int> findEvenNumbers(vector<int>& digits) {
-        set<int> ans;
-        int n = digits.size();
-        for(int i=0; i<n; i++){
-            if(digits[i] == 0){
-                continue;
-            }
-
-            for(int j=0; j<n; j++){
-                if(j == i) continue;
-
-                for(int k = 0; k<n; k++){
-                    if(k==i || k==j) continue;
-
-                    if(digits[k]%2 != 0) continue;
-
-                    int digit = digits[i]*100 + digits[j]*10+digits[k];
-
-                    ans.insert(digit);
-                }
-            }
+        vector<int> frq(10,0);
+        vector<int> ans;
+        for(int i:digits){
+            frq[i]++;
         }
 
-        return vector<int>(ans.begin(), ans.end());
+        for(int i = 100; i<=998; i+=2){
+            int a = i/100;
+            int b = (i/10)%10;
+            int c = i%10;
+
+            vector<int> req(10,0);
+            req[a]++;
+            req[b]++;
+            req[c]++;
+
+            bool possible = true;
+
+            for(int i=0; i<10; i++){
+                if(req[i]>frq[i]) {
+                    possible = false;
+                    break;
+                }
+            }
+
+            if(possible){
+                ans.push_back(i);
+            }
+        }
+        return ans;
     }
 };
